0c39f1990ee61e977c7269d68609afa2d4ef217e
FlamingTiger12 <katelyn.lin2010@gmail.com>
2025-03-19 19:31:48 -0600

    Update main.py

--- src/main/main.py
+++ src/main/main.py
@@ -1,4 +1,6 @@
-play = True
+play = True # Controls if program is running
+game = False # Controls if game is running
 while play = True:
   print("Welcome to <INSERT NAME HERE>")
+  
   play = False
