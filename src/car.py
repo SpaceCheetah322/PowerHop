cf8cf8ecd4486cc6edd06255ba40b45216746f81
SofiaStahle16 <sofiaonastahle@gmail.com>
2025-03-24 12:39:32 -0600

    Update car.py

--- src/car.py
+++ src/car.py
@@ -11,3 +11,16 @@ class Car:
 
     def checkCol(self, obj):
         pass  # Logic for checking collision with another object
+img = None
+
+def setup():
+    # Load the PNG image from the data folder
+    global img
+    img = loadImage("Car Piskel (1).png")  # Make sure the file is in the data folder
+    size(800, 600)  # Set the size of the window
+
+def draw():
+    # Clear the screen with a white background
+    background(255)
+
+    image(img, 200, 200)
