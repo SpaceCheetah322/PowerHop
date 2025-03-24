2962097e16dca82b3729d2cbef2d4202c2491d4a
FlamingTiger12 <katelyn.lin2010@gmail.com>
2025-03-24 11:56:24 -0600

    Update fly.py

--- src/fly.py
+++ src/fly.py
@@ -1,4 +1,7 @@
 import math
+import time
+import random
+
 class Fly:
     def __init__(self, image_path):
         self.x = 0
@@ -9,10 +12,16 @@ class Fly:
     def spawn(self):
         pass  # Logic for spawning the fly
 
+    def move(x, y):
+        time.sleep(7)
+        x += random.randint(0,15)
+        x += random.randint(0,15)
+
     def checkCol(self, player):
         # player_loc = [player.x, player.y]
         # loc = [x, y]
         # distance = math.dist(player_loc, loc)
         # if (distance <= 5):
         #     main.points += 5
+        #     DELETE FLY
         pass  # Logic for checking collision with the player
