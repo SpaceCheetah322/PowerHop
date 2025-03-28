da4f5c0cfded45d4fcfc41dbe4dd6ef0674f03af
SofiaStahle16 <sofiaonastahle@gmail.com>
2025-03-28 12:53:35 -0600

    Update car.py

--- src/powerHop/car.py
+++ src/powerHop/car.py
@@ -1,26 +1,39 @@
 class Car:
-    def __init__(self, x, y, speed, direction, image_path):
+    import random
+    
+    def __init__(self, x, y, speed, direction,num):
         self.x = x
         self.y = y
+        self.width = 50
+        self.height = 30
         self.speed = speed
         self.direction = direction
-        self.image = image_path  # Placeholder for the image
+        self.num = random.choice([1, 2, 3])
+
+
+
 
     def move(self):
-        pass  # Logic for car movement
+        if self.direction == 'right':
+            self.x += self.speed
+        elif self.direction == 'left':
+            self.x -= self.speed
 
-    def checkCol(self, obj):
-        pass  # Logic for checking collision with another object
+
+    def check_collision(self, frog):
+        if frog.x < self.x + self.width and frog.x + frog.width > self.x and frog.y < self.y + self.height and frog.y + frog.height > self.y:
+            return True
+        return False
 img = None
 
 def setup():
-    # Load the PNG image from the data folder
+
     global img
     img = loadImage("Car Piskel (1).png")  # Make sure the file is in the data folder
     size(800, 600)  # Set the size of the window
 
 def draw():
-    # Clear the screen with a white background
+
     background(255)
 
     image(img, 200, 200)
