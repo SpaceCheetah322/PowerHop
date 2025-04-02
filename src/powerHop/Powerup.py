2ec3fdf6a30b9c8666da560cb2d91e059e4aa0b1
FlamingTiger12 <katelyn.lin2010@gmail.com>
2025-04-02 11:55:02 -0600

    Update Powerup.py

--- src/powerHop/Powerup.py
+++ src/powerHop/Powerup.py
@@ -13,27 +13,16 @@ class Powerup:
         self.type = type
         self.x = random.randint(500, 500)
         self.y = random.randint(500, 500)
-        self.time_slow = loadImage("Frogger_Fly_Frame1.gif")
-        self.double_points = loadImage("Frogger_Fly_Frame2.gif")
-        self.health_bonus = loadImage("Frogger_Fly_Frame3.gif")
+        # Graphics WIP
+        self.time_slow = loadImage()
+        self.double_points = loadImage()
+        self.health_bonus = loadImage()
 
     # Methods
-    def display(self, type): # Displays fly; might house GIF frames someday
-        image(self.frame_1, self.x, self.y)
-
-    def move(self): # Moves fly towards random location. If location reached, wait a couple seconds and choose a new target.
-        if (self.x < self.x_loc):
-            self.x += self.x_speed
-        elif (self.x > self.x_loc):
-            self.x -= self.x_speed
-        if (self.y < self.y_loc):
-            self.y += self.y_speed
-        elif (self.y > self.y_loc):
-            self.y -= self.y_speed
-        if (self.x == self.x_loc and self.y == self.y_loc):
-            self.x_loc = random.randint(0, 500)
-            self.y_loc = random.randint(0, 500)
-            time.sleep(2)
-        self.display()
-        self.x_speed = random.randint(1, 3)
-        self.y_speed = random.randint(1, 3)
+    def display(self): # Displays fly; might house GIF frames someday
+        if self.type == 'a': 
+            image(, self.x, self.y)
+        elif self.type == 'a': 
+            image(, self.x, self.y)
+        elif self.type == 'a': 
+            image(, self.x, self.y)
