e71232f54ce3b965d6ab1630469d76e5111a34c7
FlamingTiger12 <katelyn.lin2010@gmail.com>
2025-04-02 12:32:50 -0600

    Update Powerup.py

--- src/powerHop/Powerup.py
+++ src/powerHop/Powerup.py
@@ -15,14 +15,14 @@ class Powerup:
         self.y = random.randint(500, 500)
         # Graphics WIP
         self.time_slow = loadImage("Frogger_Clock_Powerup.gif")
-        self.double_points = loadImage()
-        self.health_bonus = loadImage()
+        self.double_points = loadImage("Frogger_Clock_Powerup.gif")
+        self.health_bonus = loadImage("Frogger_Clock_Powerup.gif")
 
     # Methods
     def display(self): # Displays fly; might house GIF frames someday
         if self.type == 'a': 
-            image(, self.x, self.y)
-        elif self.type == 'a': 
-            image(, self.x, self.y)
-        elif self.type == 'a': 
-            image(, self.x, self.y)
+            image(time_slow, self.x, self.y)
+        elif self.type == 'b': 
+            image(double_points, self.x, self.y)
+        elif self.type == 'c': 
+            image(health_bonus, self.x, self.y)
