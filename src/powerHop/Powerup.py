e9abbe5964569a0e0c4adf9881c8fc5c4a68f000
FlamingTiger12 <katelyn.lin2010@gmail.com>
2025-04-02 12:40:06 -0600

    Update Powerup.py

--- src/powerHop/Powerup.py
+++ src/powerHop/Powerup.py
@@ -11,8 +11,8 @@ class Powerup:
     # Constructor
     def __init__(self, type):
         self.type = type
-        self.x = random.randint(500, 500)
-        self.y = random.randint(500, 500)
+        self.x = random.randint(0, 500)
+        self.y = random.randint(0, 500)
         # Graphics WIP
         self.time_slow = loadImage("Frogger_Clock_Powerup.gif")
         self.double_points = loadImage("Frogger_Clock_Powerup.gif")
