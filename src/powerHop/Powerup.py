4c5384667d2fbc7065a1b7f6291be92f19a8765d
FlamingTiger12 <katelyn.lin2010@gmail.com>
2025-04-02 11:53:11 -0600

    Update Powerup.py

--- src/powerHop/Powerup.py
+++ src/powerHop/Powerup.py
@@ -1,12 +1,39 @@
+"""
+Ideas for Powerups:
+a. Slow time
+b. Point X2
+c. Extra life
+"""
+import time
+import random
+
 class Powerup:
-    def __init__(self, x, y, effect, image_path):
-        self.x = x
-        self.y = y
-        self.effect = effect  # The effect of the power-up (e.g., speed boost)
-        self.image = image_path  # Placeholder for the image
+    # Constructor
+    def __init__(self, type):
+        self.type = type
+        self.x = random.randint(500, 500)
+        self.y = random.randint(500, 500)
+        self.time_slow = loadImage("Frogger_Fly_Frame1.gif")
+        self.double_points = loadImage("Frogger_Fly_Frame2.gif")
+        self.health_bonus = loadImage("Frogger_Fly_Frame3.gif")
 
-    def applyEffect(self, player):
-        pass  # Logic for applying the power-up effect to the player
+    # Methods
+    def display(self, type): # Displays fly; might house GIF frames someday
+        image(self.frame_1, self.x, self.y)
 
-    def checkCol(self, player):
-        pass  # Logic for checking collision with the player
+    def move(self): # Moves fly towards random location. If location reached, wait a couple seconds and choose a new target.
+        if (self.x < self.x_loc):
+            self.x += self.x_speed
+        elif (self.x > self.x_loc):
+            self.x -= self.x_speed
+        if (self.y < self.y_loc):
+            self.y += self.y_speed
+        elif (self.y > self.y_loc):
+            self.y -= self.y_speed
+        if (self.x == self.x_loc and self.y == self.y_loc):
+            self.x_loc = random.randint(0, 500)
+            self.y_loc = random.randint(0, 500)
+            time.sleep(2)
+        self.display()
+        self.x_speed = random.randint(1, 3)
+        self.y_speed = random.randint(1, 3)
