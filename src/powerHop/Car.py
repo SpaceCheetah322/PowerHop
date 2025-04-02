dc951a1b2a93082971c19450e4b6146d4f5cf415
AvaBaskin13 <9720852@graniteschools.org>
2025-04-02 12:17:46 -0600

    Update Car.py

--- src/powerHop/Car.py
+++ src/powerHop/Car.py
@@ -1,14 +1,13 @@
 class Car:
     import random
     
-    def __init__(self, x, y, speed, direction,num):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.width = 50
-        self.height = 30
-        self.speed = speed
-        self.direction = direction
-        self.num = random.choice([1, 2, 3])
+        global crr
+        crr = loadImage("Car.png")  
+        crr = None
+
 
 
 
@@ -24,16 +23,6 @@ class Car:
         if frog.x < self.x + self.width and frog.x + frog.width > self.x and frog.y < self.y + self.height and frog.y + frog.height > self.y:
             return True
         return False
-crr = None
-''' THIS CODE GOES INTO MAIN:
-def setup():
-
-    global crr
-    crr = loadImage("Car.png")  
-    size(800, 600)  
-
-def draw():
-
-    background(255)
-    image(crr, 200, 200, img.width*4, img.height*4)
-'''
+    def display():
+        background(255)
+        image(crr, 200, 200, img.width*4, img.height*4)
