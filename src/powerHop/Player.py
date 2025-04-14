ee02d99220b7625d1fc4ad7326b107eff2452488
KaiHale16 <155692145+SpaceCheetah322@users.noreply.github.com>
2025-04-14 12:21:25 -0600

    Update Player.py

--- src/powerHop/Player.py
+++ src/powerHop/Player.py
@@ -1,20 +1,31 @@
+import pygame
+
 class Player:
-    def __init__(self, x, y, speed, lives, image_path):
+    def __init__(self, x, y, speed, lives, image):
         self.x = x
         self.y = y
-        self.speed = 20   #needs fine tuning
+        self.speed = speed
         self.lives = lives
-        self.image = image_path
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (self.x, self.y)
 
     def move(self, keys):
-    if keys[pygame.K_LEFT]:
-        self.x -= self.speed
-    if keys[pygame.K_RIGHT]:
-        self.x += self.speed
-    if keys[pygame.K_UP]:
-        self.y -= self.speed
-    if keys[pygame.K_DOWN]:
-        self.y += self.speed
+        if keys[pygame.K_LEFT]:
+            self.x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.x += self.speed
+        if keys[pygame.K_UP]:
+            self.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.y += self.speed
+        if keys[pygame.K_a]:
+            self.x -= self.speed
+        if keys[pygame.K_d]:
+            self.x += self.speed
+        if keys[pygame.K_w]:
+            self.y -= self.speed
+        if keys[pygame.K_s]:
+            self.y += self.speed
 
-    def checkCol(self, obj):
-        pass
+        self.rect.topleft = (self.x, self.y)
