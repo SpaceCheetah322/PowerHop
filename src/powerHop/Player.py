0ae40a6d73d4335158f260fbe623cb11b18b4e33
KaiHale16 <155692145+SpaceCheetah322@users.noreply.github.com>
2025-04-18 12:39:17 -0600

    Update Player.py

--- src/powerHop/Player.py
+++ src/powerHop/Player.py
@@ -4,28 +4,21 @@ class Player:
     def __init__(self, x, y, speed, lives, image):
         self.x = x
         self.y = y
-        self.speed = speed
+        self.speed = speed = 40 #Fine tuning needed
         self.lives = lives
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.topleft = (self.x, self.y)
 
-    def move(self, keys):
-        if keys[pygame.K_LEFT]:
-            self.x -= self.speed
-        if keys[pygame.K_RIGHT]:
-            self.x += self.speed
-        if keys[pygame.K_UP]:
-            self.y -= self.speed
-        if keys[pygame.K_DOWN]:
-            self.y += self.speed
-        if keys[pygame.K_a]:
-            self.x -= self.speed
-        if keys[pygame.K_d]:
-            self.x += self.speed
-        if keys[pygame.K_w]:
-            self.y -= self.speed
-        if keys[pygame.K_s]:
-            self.y += self.speed
+    def move(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key in [pygame.K_LEFT, pygame.K_a]:
+                self.x -= self.speed
+            if event.key in [pygame.K_RIGHT, pygame.K_d]:
+                self.x += self.speed
+            if event.key in [pygame.K_UP, pygame.K_w]:
+                self.y -= self.speed
+            if event.key in [pygame.K_DOWN, pygame.K_s]:
+                self.y += self.speed
 
-        self.rect.topleft = (self.x, self.y)
+            self.rect.topleft = (self.x, self.y)
