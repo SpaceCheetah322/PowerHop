7bdcae347144f99d670e389fd07f76cd46179cd8
FlamingTiger12 <katelyn.lin2010@gmail.com>
2025-04-18 20:24:05 -0600

    Update Powerup.py

--- src/powerHop/Powerup.py
+++ src/powerHop/Powerup.py
@@ -7,6 +7,10 @@ b. Point X2
 c. Extra life
 Use the letters each powerup is assigned when calling them in, eg. 'health = Powerup("c")'
 More might be added in the future; these are some basic ones that should be easy to put in.
+
+Other Ideas:
+d. Luck boost (Increased chances of powerup/fly spawning?)
+e. Long Leap (Jump further. Not sure if this is as helpful to the player as it seems.)
 """
 import time
 import random
@@ -14,17 +18,15 @@ import random
 class Powerup:
     # Constructor
     def __init__(self, type):
-        print("Initiating")
         self.type = type
         self.x = random.randint(0, 500) # 500 is temporary! Replace with game length.
         self.y = random.randint(0, 500) # 500 is temporary! Replace with game height.
-        self.time_slow = loadImage("Frogger_Clock_Powerup.gif")
-        self.double_points = loadImage("Frogger_Point_Powerup.gif") # The four-pointed star on this one is a bit off-center. Nothing to be dfone about it though.
-        self.health_bonus = loadImage("Frogger_Health_Powerup.gif")
+        self.time_slow = loadImage("Frogger_Clock_Powerup.gif") # Shows up as a blue icon with a frozen clock.
+        self.double_points = loadImage("Frogger_Point_Powerup.gif") # Shows up as a yellow icon witih a four-pointed star. A bit off-center, nothing to be done about it though.
+        self.health_bonus = loadImage("Frogger_Health_Powerup.gif") # Shows up as a red icon with a medical (+) sign,
 
     # Methods
-    def display(self): # Displays powerup
-        print("Display entered")
+    def display(self): # Displays poweru
         if self.type == "a": 
             image(self.time_slow, self.x, self.y)
         elif self.type == "b": 
@@ -42,11 +44,9 @@ from Powerup import Powerup
 
 def setup():
     size(500, 500)
-    print("Working")
     p1 = Powerup("a")
     p2 = Powerup("b")
     p3 = Powerup("c")
-    print("Created")
     p1.display()
     p2.display()
     p3.display()
