ef6a7a9f174d984ff5eae5affd97fdecd219fbcb
KaiHale16 <155692145+SpaceCheetah322@users.noreply.github.com>
2025-04-21 12:36:44 -0600

    Update Player.py

--- src/powerHop/Player.py
+++ src/powerHop/Player.py
@@ -20,3 +20,26 @@ class Player:
 
     def display(self):
         image(self.img, self.x, self.y)
+
+
+
+"""
+from Player import Player
+
+def setup():
+    global player, frog_img
+    size(500, 500)
+    frameRate(30)
+    
+    frog_img = loadImage("Frogger_Frog_Front_Two.gif")
+    player = Player(100, 100, 40, 3, frog_img)
+
+
+def draw():
+    background(187, 185, 195)
+    player.display()
+
+
+def keyPressed():
+    player.move(keyCode)
+"""
