c0b3e4303a7336f359d8db1d522a942a060a58f9
SofiaStahle16 <sofiaonastahle@gmail.com>
2025-04-21 12:36:12 -0600

    Update Car.py

--- src/powerHop/Car.py
+++ src/powerHop/Car.py
@@ -1,24 +1,48 @@
 class Car:
-    import random
-    
-    def __init__(self, x, y):
+    def __init__(self, x, y, direction="right", speed=5, vehicle_type="car"):
         self.x = x
         self.y = y
-        global crr
-        crr = loadImage("Car.png")  
-        crr = None
+        self.direction = direction
+        self.speed = speed
+        self.vehicle_type = vehicle_type
+
+
+        image_file = "Car.png"
+        if self.vehicle_type == "truck":
+            image_file = "Truck.png"
+
+        self.image = Image(image_file)
+        
+
+        scale = 0.2 if self.vehicle_type == "car" else 0.3
+        self.image.set_size(self.image.get_width() * scale, self.image.get_height() * scale)
+        self.image.set_position(self.x, self.y)
+        add(self.image)
+
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
 
     def move(self):
         if self.direction == 'right':
             self.x += self.speed
+            if self.x > 800:
+                self.x = -self.width
         elif self.direction == 'left':
             self.x -= self.speed
+            if self.x + self.width < 0:
+                self.x = 800
 
+        self.image.set_position(self.x, self.y)
 
     def check_collision(self, frog):
-        if frog.x < self.x + self.width and frog.x + frog.width > self.x and frog.y < self.y + self.height and frog.y + frog.height > self.y:
+        if (frog.x < self.x + self.width and
+            frog.x + frog.width > self.x and
+            frog.y < self.y + self.height and
+            frog.y + frog.height > self.y):
             return True
         return False
-    def display():
-       # background(255)
-        image(crr, 200, 200, img.width*4, img.height*4)
+
+    def display(self):
+        if self.image not in get_elements():
+            add(self.image)
+        self.image.set_position(self.x, self.y)
