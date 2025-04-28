c6bd55db56772a6ba778a8ced1ed863d91fbb210
KaiHale16 <155692145+SpaceCheetah322@users.noreply.github.com>
2025-04-28 12:00:06 -0600

    Update Player.py

--- src/powerHop/Player.py
+++ src/powerHop/Player.py
@@ -15,8 +15,6 @@ class Player:
         self.img = img
         self.width = img.width
         self.height = img.height
-        #self.width = width
-        #self.height = height
 
     def move(self, key_code):
         if key_code == LEFT or key == 'a':
@@ -46,25 +44,47 @@ from Player import Player
 from Fly import Fly
 
 def setup():
-    global player, frog_img, fly_one
+    global player, frog_img, fly_one, score, fly_respawn_timer, fly_respawn_delay
     size(500, 500)
     frameRate(30)
     frog_img = loadImage("Frogger_Frog_Front_Two.gif")
     fly_one = Fly()
     player = Player(width/2-20, 450, 40, 3, frog_img)
     print(fly_one.frame_1)
+    score = 0
+    fly_respawn_timer = 0
+    fly_respawn_delay = 0
 
 
 def draw():
+    global player, fly_one, score, fly_respawn_timer, fly_respawn_delay
     background(2, 33, 84)
     fill(177, 24, 219)
     noStroke()
-    rect(0,420,500,80)
-    fly_one.move()
+    rect(0, 420, 500, 80)
+
+    if fly_one is not None:
+            fly_one.move()
+            if player.collides_with(fly_one):
+                score += 10
+                fly_one = None
+                fly_respawn_timer = frameCount  # record when it was eaten
+                fly_respawn_delay = int(random(270, 330))  # random 9-11 seconds (30 fps * 9-11 sec)
+
+    else:
+        # Check if enough time passed
+        if frameCount - fly_respawn_timer > fly_respawn_delay:
+            fly_one = Fly()  # spawn new fly!
+
     player.display()
+    print(score)
+
+
+        
 
 
 def keyPressed():
     player.move(keyCode)
+
 
 """
