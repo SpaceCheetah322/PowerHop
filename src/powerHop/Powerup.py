bc21adba17a1a984c9c61194a024fd8530b68d51
Katelyn <katelyn.lin2010@gmail.com>
2025-04-28 12:29:03 -0600

    Update Powerup.py

--- src/powerHop/Powerup.py
+++ src/powerHop/Powerup.py
@@ -1,5 +1,5 @@
 # Written by Katelyn
-# NOTES: Display is functioning! Powerup should appear in a random location when called. Collision detection!
+# NOTES: Display is functioning! Powerup should appear in a random location when called. Collision detection also functional!
 # Might end up having to resize graphics.
 """
 Powerups:
